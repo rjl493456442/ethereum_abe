9ed54825c6880ba9f91aaeda06445862261b4d5b
gary rong <garyrong0905@gmail.com>
2016-09-06 15:53:24 +0800

    ??

--- scripts/tools.py
+++ scripts/tools.py
@@ -12,6 +12,6 @@ class Tool(object):
             cnt = self.db_proxy.get_table_count(FLAGS.txs)
             for idx in range(cnt):
                 time_start = time.time()
-                self.db_proxy.insert(FLAGS.txs+"test"+str(idx/10), self.dbproxy.get(FLAGS.txs+str(idx), None, multi=True))
+                self.db_proxy.insert(FLAGS.txs+"test"+str(idx/10), self.db_proxy.get(FLAGS.txs+str(idx), None, multi=True))
                 print "elapsed %f" % (time.time() - time_start)
                         
